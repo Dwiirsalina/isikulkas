5d98c8fbe218ccdf7c8d4ee011a7d52c79c0daeb
Unknown <rezkyal2@gmail.com>
2018-04-21 21:19:49 +0700

    revisi model

--- database/migrations/2018_04_21_083512_create_resep_table.php
+++ database/migrations/2018_04_21_083512_create_resep_table.php
@@ -15,15 +15,16 @@ class CreateResepTable extends Migration
     public function up()
     {
         Schema::create($this->tablename, function (Blueprint $table) {
-            $table->uuid('id');
+            $table->increments('id');
             $table->string('nama_masakan')->nullable();
-            $table->string('bahan_masakan')->nullable();
+            $table->string('bahan_masakan',200)->nullable();
             $table->string('kat_masakan')->nullable();
-            $table->string('cara_masakan')->nullable();
-            $table->string('alat_masakan')->nullable();
+            $table->string('cara_masakan',1000)->nullable();
+            $table->string('alat_masakan',200)->nullable();
             $table->string('jumlah_bahan_masakan')->nullable();
             $table->string('satuan_bahan_masakan')->nullable();
             $table->string('ukuran_bahan_masakan')->nullable();
+            $table->string('imageUrl')->nullable();
             $table->char('rating')->nullable();
             $table->timestamps();
         });
